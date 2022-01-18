44da5123feddfa1521f71c3e397aa4d3dddcd923
zhangpc <zhangpeichuan@126.com>
2022-01-18 13:21:15 +0800

    linkedlist insert tail

--- datastructures/04_linked_list_implement.c
+++ datastructures/04_linked_list_implement.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
+typedef struct LinkedNode{
     int data;
-    struct Node* next;
+    struct LinkedNode* next;
 }LinkedList;
 
+LinkedList* createLinkedNode(int value){
+    LinkedList *tmp = (LinkedList *)malloc(sizeof(LinkedList));
+    tmp->data = value;
+    tmp->next = NULL;
+    return tmp;
+}
+void insertTail(const LinkedList* list,const LinkedList* node)
+{
+    LinkedList *tmp = (LinkedList *)list;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = (LinkedList *)node;
+}
+void traverLinkedList(const LinkedList* list){
+    LinkedList *tmp = (LinkedList *)list;
+    while (tmp->next != NULL)
+    {
+        printf("address %p value %d \n",tmp,tmp->data);
+        tmp = tmp->next;
+    }
+}
 int main()
 {   
     LinkedList* A = NULL;
-    LinkedList *tmp = (LinkedList *)malloc(sizeof(LinkedList));
-    tmp->data = 1;
-    tmp->next = NULL;
-    A->next = tmp;
-    LinkedList *traver = A;
-    do
-    {   
-        printf("address %p %d ",traver,traver->data);
-        traver = traver->next;
-    } while (traver->next != NULL);
-    
-    
-    return 0;
+    for (int i = 0; i < 10; i++)
+    {
+        LinkedList* tmp = createLinkedNode(i+1);
+        if (i == 0) {
+            A = tmp;
+        }else{
+            insertTail(A,tmp);
+        }
+        
+    }
+    traverLinkedList(A);
 }
