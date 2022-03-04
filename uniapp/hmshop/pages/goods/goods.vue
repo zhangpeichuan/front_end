e504cd462a3f7a5d332156af9197a27e8df2c971
zhangpc <zhangpeichuan@126.com>
2022-03-04 11:31:39 +0800

    下拉刷新和上拉加载

--- uniapp/hmshop/pages/goods/goods.vue
+++ uniapp/hmshop/pages/goods/goods.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="goods_list">
 		<goods-list :goods="goods"></goods-list>
+		<view class="isOver" v-if="flag">------我是有底线的------</view>
 	</view>
 
 </template>
@@ -11,30 +12,64 @@
 		data() {
 			return {
 				pageindex:1,
-				goods:[]
+				goods:[],
+				flag:false
 			}
 		},
 		components: {"goods-list":goodsList},
 		methods: {
 			//获取商品列表数据
-			async getGoodsList(){
-				console.log('getGoodsList')
+			async getGoodsList(callback){
+				console.log('getGoodsList ')				
 				const res = await this.$myrequest({
-					url:'/api/getgoods?pageinde='+this.pageindex
+					url:'/api/getgoods?pageindex='+this.pageindex
 				})
 				console.log(res)
-				this.goods=res.data.message
+				if(this.pageindex == 1){
+					this.goods = res.data.message
+				}else{
+					this.goods = [...this.goods,...res.data.message]
+				}
+				callback && callback()
 			},
 			
 		},
 		onLoad() {
 			this.getGoodsList()
 		},
+		onReachBottom() {
+			console.log('onReachBottom')
+			if(this.goods.length < this.pageindex*10){
+				this.flag = true
+				return
+			}
+			this.pageindex++
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			console.log('onPullDownRefresh')
+			this.pageindex = 1;
+			this.flag = false
+			this.goods = []
+			setTimeout(()=>{
+				this.getGoodsList(()=>{
+					uni.stopPullDownRefresh()
+				})
+			},1000)
+			
+		}
 	}
 </script>
 
 <style lang="scss">
 	.goods_list{
 		background-color: #eee;
+	}
+	.isOver{
+		width: 100%;
+		height: 50px;
+		line-height: 50px;
+		text-align: center;
+		font-size: 28;
 	}
 </style>
