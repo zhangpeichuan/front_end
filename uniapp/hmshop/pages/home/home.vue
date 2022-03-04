a85860c8dd6e032caced20d740e2ad32b2dc39e0
zhangpc <zhangpeichuan@126.com>
2022-03-04 09:15:22 +0800

    首页轮播和导航图片

--- uniapp/hmshop/pages/home/home.vue
+++ uniapp/hmshop/pages/home/home.vue
@@ -1,6 +1,32 @@
 <template>
-	<view>
-	首页
+	<view class="home" >
+		<!-- 轮播图 -->
+	<swiper indicator-dots="true" circular="true" autoplay="true">
+		<swiper-item v-for="item in swipers" :key="item.id"><image :src="item.img"></image></swiper-item>		
+	</swiper>
+	<!-- 导航按钮区域 -->
+	<view class="nav">
+		<view class="nav_item">
+			<view class="iconfont icon-ziyuan">
+			</view>
+			<text>黑马超市</text>
+		</view>
+		<view class="nav_item">
+			<view class="iconfont icon-guanyuwomen">
+			</view>
+			<text>联系我们</text>
+		</view>
+		<view class="nav_item">
+			<view class="iconfont icon-tupian">
+			</view>
+			<text>社区图片</text>
+		</view>
+		<view class="nav_item">
+			<view class="iconfont icon-shipin">
+			</view>
+			<text>学习视频</text>
+		</view>
+	</view>
 	</view>
 </template>
 
@@ -45,6 +71,38 @@
 	}
 </script>
 
-<style>
-	
+<style lang="scss">
+	.home{
+		swiper{
+			width: 750rpx;
+			height: 380rpx;
+			image{
+				height: 100%;
+				width: 100%;
+			}
+		}
+	}
+	.nav{
+		display: flex;
+		.nav_item{
+			width: 25%;
+			text-align: center;
+			view{
+				width: 120rpx;
+				height: 120rpx;
+				background-color: #b50e03;
+				border-radius: 60rpx;
+				margin: 10px auto;
+				line-height: 120rpx;
+				color: #ffffff;
+				font-size: 50rpx;
+			}
+			.icon-tupian{
+				font-size: 45rpx;
+			}
+			text{
+				font-size: 30rpx;
+			}
+		}
+	}
 </style>
