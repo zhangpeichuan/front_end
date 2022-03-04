0f5e43781307e331bdc5d6dc0b0f43e5d3a710c3
zhangpc <zhangpeichuan@126.com>
2022-03-04 15:30:37 +0800

    咨询内容展示和组件微调

---
+++ uniapp/hmshop/compontent/news-item.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+	<view class="new_item" v-for="item in list" :key="item.id">
+		<image :src="item.img_url"></image>
+		<view class="right">
+			<view class="title">
+				{{item.title}}
+			</view>
+			<view class="info">
+				<text>发表时间:{{item.add_time}}</text>
+				<text>浏览次数:{{item.click}}</text>
+			</view>
+		</view>
+	</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:['list']
+	}
+</script>
+
+<style lang="scss">
+.new_item{
+			display: flex;
+			padding: 10rpx 20rpx;
+			border-bottom: 1px solid $shop-color;
+			image{
+				min-width: 200rpx;
+				max-width: 200rpx;
+				height: 150rpx;
+				
+			}
+			.right{
+				margin-left: 15rpx;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+				.titile{
+					font-size: 30rpx;
+				}
+				.info{
+					font-size: 24rpx;
+					text:nth-child(2){
+						margin-left: 30rpx;
+					}
+				}
+			}
+		}
+</style>
--- uniapp/hmshop/pages/news/news.vue
+++ uniapp/hmshop/pages/news/news.vue
@@ -1,9 +1,35 @@
 <template>
-	<view>资讯</view>
+	<view class="news">
+	<news-item :list="newsList"></news-item>
+	</view>
 </template>
 
 <script>
+	import newsItem from '../../compontent/news-item.vue'
+	export default {
+		data() {
+			return {
+				newsList:[]
+			}
+		},
+		components:{"news-item":newsItem},
+		onLoad() {
+			this.getNews()
+		},
+		methods:{
+			async getNews(){
+				const res = await this.$myrequest({
+					url:'/api/getnewslist'
+				})
+				console.log(res)
+				this.newsList = res.data.message
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="scss">
+	.news{
+		
+	}
 </style>
