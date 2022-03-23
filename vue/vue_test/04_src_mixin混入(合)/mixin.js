a8108742efc57a53d03e4103f5f1f556ba357d3f
zhangpc <zhangpeichuan@126.com>
2022-03-23 17:48:20 +0800

    mixin混入

---
+++ vue/vue_test/04_src_mixin混入(合)/mixin.js
@@ -0,0 +1,18 @@
+export const mixin1 = {
+  methods: {
+    showName(){
+      alert(this.name)
+    }
+  },
+  mounted() {
+    console.log('你好啊')
+  },
+}
+export const mixin2 = {
+  data() {
+    return {
+      x: 100,
+      y: 200
+    }
+  },
+}
--- vue/vue_test/src/components/School.vue
+++ vue/vue_test/src/components/School.vue
@@ -1,19 +1,20 @@
 <template>
 	<div	class="demo">
-		<h2 @click="showName">学生名称：{{name}}</h2>
-		<h2>学生性别：{{sex}}</h2>
+		<h2 @click="showName">学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
 	</div>
 </template>
 
 <script>
 // import {mixin1,mixin2} from '../mixin'
-export	default {
-		name:'Student',
+export	default{
+		name:'School',
 		data(){
 			console.log(this)
 			return{
-				name:'张三',
-				sex:'男'
+				name:'尚硅谷',
+				address:'北京',
+				x: 333
 			}
 		},
 		// mixins:[mixin1,mixin2]
